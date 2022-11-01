82403c58ce134092fe93a80e7e0139ee75a8863b
Nikola Prokopić <nikola@prokopic.rs>
2022-11-01 17:10:49 +0100

    Extract common reconcile delete request

--- pkg/aws/subnets/reconciler_delete.go
+++ pkg/aws/subnets/reconciler_delete.go
@@ -4,34 +4,13 @@ import (
 	"context"
 
 	"github.com/giantswarm/microerror"
-	"sigs.k8s.io/cluster-api/util/conditions"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/giantswarm/aws-vpc-operator/pkg/aws"
 	"github.com/giantswarm/aws-vpc-operator/pkg/errors"
 )
 
-// ReconcileDeleteRequest specified which resource is being deleted and what is
-// the specification of the subnets that should be deleted.
-type ReconcileDeleteRequest struct {
-	// Resource that is being reconciled.
-	Resource conditions.Setter
-
-	// Spec of the desired subnets.
-	Spec DeletedSpec
-}
-
-type DeletedSpec struct {
-	ClusterName string
-	RoleARN     string
-	Region      string
-	Subnets     []DeletedSubnetSpec
-}
-
-type DeletedSubnetSpec struct {
-	SubnetId string
-}
-
-func (r *reconciler) ReconcileDelete(ctx context.Context, request ReconcileDeleteRequest) (err error) {
+func (r *reconciler) ReconcileDelete(ctx context.Context, request aws.ReconcileDeleteAllRequest) (err error) {
 	logger := log.FromContext(ctx)
 	logger.Info("Started reconciling subnets deletion")
 	defer func() {
@@ -42,27 +21,25 @@ func (r *reconciler) ReconcileDelete(ctx context.Context, request ReconcileDelet
 		}
 	}()
 
-	spec := request.Spec
-
-	if spec.ClusterName == "" {
+	if request.ClusterName == "" {
 		return microerror.Maskf(errors.InvalidConfigError, "ClusterName must not be empty")
 	}
-	if spec.RoleARN == "" {
+	if request.RoleARN == "" {
 		return microerror.Maskf(errors.InvalidConfigError, "RoleARN must not be empty")
 	}
-	if spec.Region == "" {
+	if request.Region == "" {
 		return microerror.Maskf(errors.InvalidConfigError, "Region must not be empty")
 	}
-	if len(spec.Subnets) == 0 {
-		return microerror.Maskf(errors.InvalidConfigError, "Subnets must not be empty")
+	if len(request.Specs) == 0 {
+		return microerror.Maskf(errors.InvalidConfigError, "Specs must not be empty")
 	}
 
 	deleteSubnetsInput := DeleteSubnetsInput{
-		RoleARN: request.Spec.RoleARN,
-		Region:  request.Spec.Region,
+		RoleARN: request.RoleARN,
+		Region:  request.Region,
 	}
-	for _, subnetSpec := range request.Spec.Subnets {
-		deleteSubnetsInput.SubnetIds = append(deleteSubnetsInput.SubnetIds, subnetSpec.SubnetId)
+	for _, spec := range request.Specs {
+		deleteSubnetsInput.SubnetIds = append(deleteSubnetsInput.SubnetIds, spec.Id)
 	}
 	err = r.client.Delete(ctx, deleteSubnetsInput)
 	if err != nil {

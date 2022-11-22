13c6122142b149ec3ac2cf7aa5b77f3cdb13c2bf
Nikola Prokopić <nikola@prokopic.rs>
2022-11-22 17:21:06 +0100

    Move VPC endpoint update logic from client to reconciler

--- pkg/aws/vpcendpoint/client_update.go
+++ pkg/aws/vpcendpoint/client_update.go
@@ -2,7 +2,6 @@ package vpcendpoint
 
 import (
 	"context"
-	"sort"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
@@ -14,14 +13,14 @@ import (
 )
 
 type UpdateVpcEndpointInput struct {
-	RoleARN                 string
-	Region                  string
-	VpcEndpointId           string
-	CurrentSubnetIds        []string
-	CurrentSecurityGroupIds []string
-	WantedSubnetIds         []string
-	WantedSecurityGroupIds  []string
-	Tags                    map[string]string
+	RoleARN                string
+	Region                 string
+	VpcEndpointId          string
+	AddSecurityGroupIds    []string
+	AddSubnetIds           []string
+	RemoveSecurityGroupIds []string
+	RemoveSubnetIds        []string
+	Tags                   map[string]string
 }
 
 func (c *client) Update(ctx context.Context, input UpdateVpcEndpointInput) (err error) {
@@ -44,50 +43,21 @@ func (c *client) Update(ctx context.Context, input UpdateVpcEndpointInput) (err 
 	if input.VpcEndpointId == "" {
 		return microerror.Maskf(errors.InvalidConfigError, "%T.VpcId must not be empty", input)
 	}
-	if len(input.WantedSubnetIds) == 0 {
-		return microerror.Maskf(errors.InvalidConfigError, "%T.WantedSubnetIds must not be empty", input)
-	}
-	if len(input.WantedSecurityGroupIds) == 0 {
-		return microerror.Maskf(errors.InvalidConfigError, "%T.WantedSecurityGroupIds must not be empty", input)
-	}
 
-	// Sort current securityGroupIds and subnetIds, so we can use sort.SearchStrings
-	// when checking difference in slices.
-	// This modifies slice in-place, but we just use it here anyway, so that's
-	// fine.
-	sort.Strings(input.CurrentSecurityGroupIds)
-	sort.Strings(input.CurrentSubnetIds)
-
-	// securityGroupIDs that we will add, those specified in the input, but not
-	// already present in current state
-	securityGroupIdsToBeAdded := diff(input.WantedSecurityGroupIds, input.CurrentSecurityGroupIds)
-
-	// securityGroupIDs that we will remove, those already in the current state,
-	// but not present in the input
-	securityGroupIdsToBeRemoved := diff(input.CurrentSecurityGroupIds, input.WantedSecurityGroupIds)
-
-	// subnets that we will add, those specified in the input, but not already
-	// present in current state
-	subnetIdsToBeAdded := diff(input.WantedSubnetIds, input.CurrentSubnetIds)
-
-	// subnets that we will remove, those already in the current state, but not
-	// present in the input
-	subnetIdsToBeRemoved := diff(input.CurrentSubnetIds, input.WantedSubnetIds)
-
-	if atLeastOneIsNotEmpty(securityGroupIdsToBeAdded, securityGroupIdsToBeRemoved, subnetIdsToBeAdded, subnetIdsToBeRemoved) {
+	if atLeastOneIsNotEmpty(input.AddSecurityGroupIds, input.AddSubnetIds, input.RemoveSecurityGroupIds, input.RemoveSubnetIds) {
 		logger.Info("VPC endpoint needs updates",
 			"vpc-endpoint-id", input.VpcEndpointId,
-			"add-security-groups", securityGroupIdsToBeAdded,
-			"remove-security-groups", securityGroupIdsToBeRemoved,
-			"add-subnets", subnetIdsToBeAdded,
-			"remove-subnets", subnetIdsToBeRemoved)
+			"add-security-groups", input.AddSecurityGroupIds,
+			"remove-security-groups", input.RemoveSecurityGroupIds,
+			"add-subnets", input.AddSubnetIds,
+			"remove-subnets", input.RemoveSubnetIds)
 
 		ec2Input := ec2.ModifyVpcEndpointInput{
 			VpcEndpointId:          aws.String(input.VpcEndpointId),
-			AddSecurityGroupIds:    securityGroupIdsToBeAdded,
-			AddSubnetIds:           subnetIdsToBeAdded,
-			RemoveSecurityGroupIds: securityGroupIdsToBeRemoved,
-			RemoveSubnetIds:        subnetIdsToBeRemoved,
+			AddSecurityGroupIds:    input.AddSecurityGroupIds,
+			AddSubnetIds:           input.AddSubnetIds,
+			RemoveSecurityGroupIds: input.RemoveSecurityGroupIds,
+			RemoveSubnetIds:        input.RemoveSubnetIds,
 			ResetPolicy:            aws.Bool(true),
 		}
 		_, err = c.ec2Client.ModifyVpcEndpoint(ctx, &ec2Input, c.assumeRoleClient.AssumeRoleFunc(input.RoleARN, input.Region))
@@ -113,27 +83,6 @@ func (c *client) Update(ctx context.Context, input UpdateVpcEndpointInput) (err 
 	}
 
 	return nil
-}
-
-// diff returns all values from sortedS1 and not present in sortedS2.
-//
-// Example:
-//
-//	["a", "b", "c", "d"] - ["a", "c", "e", "f"] = ["b", "d"]
-func diff(sortedS1, sortedS2 []string) []string {
-	var result []string
-
-	for _, s := range sortedS1 {
-		i := sort.SearchStrings(sortedS2, s)
-		if i < len(sortedS2) && sortedS2[i] == s {
-			// string s from sortedS1 found in sortedS2 at index i
-			continue
-		} else {
-			result = append(result, s)
-		}
-	}
-
-	return result
 }
 
 func atLeastOneIsNotEmpty(slices ...[]string) bool {

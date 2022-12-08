a9ad88d342325f8ade6066639db78a5b00b208fe
calvix <vaclav@giantswarm.io>
2022-12-08 11:33:30 +0100

    add-capa-tags-to-subnets

--- pkg/aws/tags/builder.go
+++ pkg/aws/tags/builder.go
@@ -7,9 +7,6 @@ import (
 const (
 	NameAWSProviderPrefix = "github.com/giantswarm/aws-vpc-operator/"
 	NameAWSRole           = NameAWSProviderPrefix + "role"
-
-	CAPARole  = "sigs.k8s.io/cluster-api-provider-aws/role"
-	CAPAOwned = "sigs.k8s.io/cluster-api-provider-aws/cluster/"
 )
 
 // BuildParams is used to build tags around an aws resource.
@@ -51,8 +48,7 @@ func (p BuildParams) Build() map[string]string {
 		tags["Name"] = p.Name
 	}
 
-	tags[CAPARole] = p.Role
-	tags[CAPAOwned+p.ClusterName] = "owned"
+	tags[NameAWSProviderPrefix+p.ClusterName] = "owned"
 
 	// CAPA is also setting "cluster/$cluster_id=owned" tag which makes the
 	// resource to be managed by CAPA, so we skip that here.
